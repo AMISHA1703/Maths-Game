d5a32449cbecb9c14e9c6df7e158a88c971b2bdc
Amisha Singh <amishakumari1703@gmail.com>
2024-03-13 21:08:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var box1=documnet.queryselector("box1");
+var box2=documnet.queryselector("box2");
+var box3=documnet.queryselector("box3");
+
+var playing=false;
+var score;
+
+// set start btn
+document.getElementsById("ans").onclick()=function(){
+    if(playing==true){
+        
+    }
+}
