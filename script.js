77c88fa73ce5d2647a4045d1aeab5f3b738405e0
Amisha Singh <amishakumari1703@gmail.com>
2024-03-14 00:10:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var box1=documnet.queryselector("box1");
-var box2=documnet.queryselector("box2");
-var box3=documnet.queryselector("box3");
+
 
 var playing=false;
 var score;
 
 // set start btn
-document.getElementsById("ans").onclick()=function(){
+document.getElementById("ans").onclick=function(){
     if(playing==true){
+        location.reload();
+    
+    }
+
+    else{
+        score=0;
+        document.getElementById("scorevalue").innerHTML=score;
         
     }
 }
